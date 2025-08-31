c641856dc0bc6675d592402c8c405b1f409a254c
Byongho <unlike96@gmail.com>
2025-08-31 16:32:20 +0900

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/senior-web-magazine">
       <App />
     </BrowserRouter>
   </StrictMode>,
